403556103f7307b242b957039383482c1deb9521
Vicenç García-Altés <vgaltes@gmail.com>
2014-05-04 23:24:53 +0200

    added support for jshint

--- Jakefile.js
+++ Jakefile.js
@@ -1,4 +1,13 @@
-desc("Example!")
+task("default", ["lint"]);
+
+desc("Lint everything");
+task ("lint", [], function()
+{
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("Jakefile.js", {}, {});
+});
+
+desc("Example!");
 task("example", ["dependency"], function(){
    console.log("example task");
 });
