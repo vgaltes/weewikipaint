fb5005a0dd2768a6b4bd36f9b204a74325cf9d78
Vicenç García-Altés <vgaltes@gmail.com>
2014-05-18 13:43:26 +0200

    Ensure a port is given to start method

--- src/server/_server_tests.js
+++ src/server/_server_tests.js
@@ -1,29 +1,47 @@
 "use strict";
 
+var PORT = 8080;
 var server = require("./server.js");
 var http  = require("http");
 
-exports.tearDown = function(cb){
-    server.stop(function(){
-    });
-    cb();
-};
-
 exports.test_serverReturnHelloWorld = function(test){
-    server.start();
-
+    server.start(PORT);
     var request = http.get("http://localhost:8080");
     request.on("response", function(response){
         var recievedData = false;
         response.setEncoding("utf-8");
         response.on("data", function(chunk){
-            recievedData = true;
             test.equals("Hello world", chunk, "response text");
         });
         response.on("end", function(){
             test.equals(200, response.statusCode, "status code");
-            test.ok(recievedData, "data received");
+            server.stop(function(){});
             test.done();
         });
     });
+};
+
+exports.test_serverRequiresPortNumber = function(test){
+    test.throws(function(){
+        server.start();
+    });
+
+    test.done();
+};
+
+exports.test_serverRunsCallbackWhenStopCompletes = function(test){
+    server.start(PORT);
+    server.stop(function(){
+        test.done();
+    });
+};
+
+exports.test_stopCalledTwiceInARowThrowsException = function(test){
+    server.start(PORT);
+    server.stop(function(){});
+    test.throws(function(){
+        server.stop(function(){});
+    });
+
+    test.done();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -3,14 +3,16 @@
 var http  = require("http");
 var server;
 
-exports.start = function(){
+exports.start = function(portNumber){
+    if(!portNumber) throw "port number is required";
+
     server = http.createServer();
     server.on("request", function(request, response){
         response.statusCode = 200;
         response.end("Hello world");
     });
 
-    server.listen(8080);
+    server.listen(portNumber);
 };
 
 exports.stop = function(callback){
