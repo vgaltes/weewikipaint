2568235138600f1779e7cdbbb844e9f645206b23
Vicenc.Garcia-Altes <vicencgarcia-altes@VicencGarcia-Altess-MacBook-Pro.local>
2014-07-08 09:56:34 +0100

    ready to publish to heroku

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -10,13 +10,17 @@
     var jake = require("jake");
     var child_process = require("child_process");
     var http = require("http");
+    var fs = require("fs");
     var child;
+    var PORT = 5000;
 
     exports.setUp = function(done) {
         runServer(done);
     };
 
     exports.tearDown = function(done) {
+        if (!child) return;
+
         child.on("exit", function(code, signal) {
             done();
         });
@@ -24,7 +28,7 @@
     };
 
     exports.test_canGetHomePage = function(test) {
-        httpGet("http://localhost:8080", function(response, receivedData) {
+        httpGet("http://localhost:" + PORT, function(response, receivedData) {
             var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
             test.ok(foundHomePage, "home page should have contained test marker");
             test.done();
@@ -32,18 +36,44 @@
     };
 
     exports.test_canGet404Page = function(test) {
-        httpGet("http://localhost:8080/nonexistant.html", function(response, receivedData) {
+        httpGet("http://localhost:" + PORT + "/nonexistant.html", function(response, receivedData) {
             var foundHomePage = receivedData.indexOf("WeeWikiPaint 404 page") !== -1;
             test.ok(foundHomePage, "404 page should have contained test marker");
             test.done();
         });	};
 
     function runServer(callback) {
-        child = child_process.spawn("node", ["src/server/weewikipaint", "8080"]);
+        var commandLine = parseProcFile();
+        child = child_process.spawn(commandLine[0], commandLine.slice(1));
         child.stdout.setEncoding("utf8");
         child.stdout.on("data", function(chunk) {
-            if (chunk.trim() === "Server started") callback();
+            console.log("stdout: " + chunk);
+            if (chunk.trim().indexOf("Server started") !== -1) callback();
         });
+        child.stderr.on("data", function(chunk) {
+            console.log("stderr: " + chunk);
+        });
+        child.on("exit", function(code, signal) {
+            console.log("child process died");
+        });
+    }
+
+    function parseProcFile() {
+        var procFile = fs.readFileSync("Procfile", "utf8");
+        var matches = procFile.trim().match(/^web:(.*)$/);     // matches 'web: foo bar baz'
+
+        if (matches === null) throw "Could not parse ProcFile";
+        var commandLine = matches[1];
+
+        var args = commandLine.split(" ");
+        args = args.filter(function(element) {
+            return (element.trim() !== "");
+        });
+        args = args.map(function(element) {
+            if (element === "$PORT") return PORT;
+            else return element;
+        });
+        return args;
     }
 
     function httpGet(url, callback) {
